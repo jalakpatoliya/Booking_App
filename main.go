9056acc54f7200c8bb5861b3b6a6b71de5ab4e4c
jalak <jalakpatoliya@gmail.com>
2024-04-01 18:46:56 +0530

    ⚡️ printing type of var

--- main.go
+++ main.go
@@ -7,8 +7,19 @@ func main()  {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to",conferenceName,"booking application")
-	fmt.Println("We have total of",conferenceTickets,"tickets and", remainingTickets,"are still available")
+
+	fmt.Printf("Type of conferenceName is %T, conferenceTickets is %T, remainingTickets is %T\n",conferenceName,conferenceTickets,remainingTickets)
+	fmt.Printf("Welcome to %v booking application \n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+
+	userName = "Ram"
+	userTickets = 2
+
+	fmt.Printf("User %v booked %v tickets \n", userName,userTickets)
 
 }
