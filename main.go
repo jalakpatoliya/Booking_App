55c2e7fca5a9e49faff8983c25748d9f3ba87e0f
jalak <jalakpatoliya@gmail.com>
2024-04-03 17:57:59 +0530

    ⚡️ using different files in package

--- main.go
+++ main.go
@@ -79,15 +79,6 @@ func getUserInputs() (string,string,string,uint)  {
 	return firstName,lastName,email, userTickets
 }
 
-func validateUserInputs(firstName string,lastName string,email string,userTickets uint) (bool,bool,bool) {
-	
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email,"@")
-	isValidTicketNumber :=  userTickets > 0 && userTickets <= remainingTickets 
-	
-	return isValidName, isValidEmail, isValidTicketNumber
-}
-
 func getFirstNames() [] string {
 	firstNames := [] string {}
 
